61e4f44141242dcbadde3e179f5e696a92c1bb3b
AndrewLPimenta <pimenta.andreews@gmail.com>
2025-01-29 00:53:32 -0300

    agora foi =)=)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';  // Página inicial
 import Present from './pages/Present';    // Página de presença
 import Registration from './pages/Registration';  // Página de registro
 
 function App() {
   return (
-    <Router> {/* Envolvendo o conteúdo com Router para habilitar a navegação */}
-      <Routes> {/* Usando Routes ao invés de Switch (para React Router v6+) */}
-      <Route path="/HomePage" element={<HomePage />} /> {/* Página inicial */}
+    <BrowserRouter> {/* Usando BrowserRouter para navegação */}
+      <Routes> {/* Definindo as rotas */}
+        <Route path="/" element={<HomePage />} /> {/* Página inicial */}
         <Route path="/Present" element={<Present />} /> {/* Página de presença */}
         <Route path="/Registration" element={<Registration />} /> {/* Página de registro */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
